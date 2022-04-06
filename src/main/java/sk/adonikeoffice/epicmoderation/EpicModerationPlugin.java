df7f22c5c71bd87e88d48c4217adf38f0eeb277d
adonikeoffice <adonikeoffice@gmail.com>
2022-04-06 14:50:00 +0200

    // Uploading starter kit

---
+++ src/main/java/sk/adonikeoffice/epicmoderation/EpicModerationPlugin.java
@@ -0,0 +1,12 @@
+package sk.adonikeoffice.epicmoderation;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public final class EpicModerationPlugin extends SimplePlugin {
+
+	@Override
+	protected void onPluginStart() {
+	
+	}
+
+}
